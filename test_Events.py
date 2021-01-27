81fbc1874d7ef42a1dba52fadc696af3f3c8ecea
Anish Tamang <anish.tmngo@gmail.com>
2021-01-26 19:46:26 -0500

    Update test_Events.py

--- test_Events.py
+++ test_Events.py
@@ -6,13 +6,13 @@ from datetime import date, datetime
 
 class BasicTestCase(unittest.TestCase):
 
-    def setUp(self):
-        unittest.TestLoader.sortTestMethodsUsing = None
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['DEBUG'] = False
-        self.app = app.test_client()
-        self.assertEqual(app.debug, False)
+#     def setUp(self):
+#         unittest.TestLoader.sortTestMethodsUsing = None
+#         app.config['TESTING'] = True
+#         app.config['WTF_CSRF_ENABLED'] = False
+#         app.config['DEBUG'] = False
+#         self.app = app.test_client()
+#         self.assertEqual(app.debug, False)
 
     def test_1_viewing(self):
         rv = self.app.get('/event-view')
